164bb9d3d9aaed4070a95dace47a15de782ad0a1
Vy-CES <vy.bui@gs.liferay.com>
2021-09-27 16:24:35 +0700

    Prompt 4.2 Do not expose liferay application port in themedisplay.
    Signed-off-by: Vy-CES <vy.bui@gs.liferay.com>

--- portal-kernel/src/com/liferay/portal/kernel/util/Portal.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/Portal.java
@@ -1079,9 +1079,18 @@ public interface Portal {
 		PortletResponse portletResponse, ThemeDisplay themeDisplay,
 		String portletName);
 
+	/**
+	 * @deprecated As of 7.0.0, replaced by {@link #getSiteAdminURL(Company,
+	 *             Group, String, Map)}
+	 */
+	@Deprecated
 	public String getSiteAdminURL(
 			Company company, Group group, String ppid,
 			Map<String, String[]> params)
+		throws PortalException;
+
+	public String getSiteAdminURL(
+		ThemeDisplay themeDisplay, String ppid, Map<String, String[]> params)
 		throws PortalException;
 
 	/**
